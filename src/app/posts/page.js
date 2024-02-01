f9ea1bb7975a76b47e6433f7b6a7640d746702b2
Saif Maamun <sadifmamun@gmail.com>
2024-02-01 10:59:25 +0600

    force-cache

---
+++ src/app/posts/page.js
@@ -0,0 +1,26 @@
+
+const Posts = async () => {
+    const res = await fetch("http://localhost:5000/posts", {
+        cache: "force-cache"
+    });
+    const posts = await res.json();
+    console.log(posts)
+    return (
+        <div className="w-full">
+            {posts.map(posts =>
+                <div key={posts.id} className="card w-9/12  bg-slate-500 mx-auto my-4 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">{posts.title}</h2>
+                        <p>{posts.description}</p>
+                        <p>Likes Count: {posts.likesCount}</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">See More</button>
+                        </div>
+                    </div>
+                </div>)}
+
+        </div>
+    );
+};
+
+export default Posts;
